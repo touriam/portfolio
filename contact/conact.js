08f56aa945ec2f4c10c4fe9a91a8820bd6e28ae5
touria <touriamt69@gmail.com>
2023-04-14 22:50:20 -0400

    added js to check for email validation

--- contact/conact.js
+++ contact/conact.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const listItem = document.querySelectorAll('.list');
 const navContainer = document.querySelector('.nav-container');
-const mediaQuery = window.matchMedia('(max-width: 800px)')
+const mediaQuery = window.matchMedia('(max-width: 800px)');
 
 
 hamburger.addEventListener("click", function(){
@@ -14,5 +14,31 @@ hamburger.addEventListener("click", function(){
     else (navContainer.style.display = "none")
     
     })
+
+const emailLabel = document.getElementById('email-label');
+const emailField = document.getElementById('email-field');
+const emailError = document.getElementById('email-error');
+
+const validation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+function emailCheck(){
+
+    if(emailField.value == ""){
+    emailError.innerHTML = "Please enter an email adress";
+    emailField.style.borderBottomColor= "red";
+    return false;
+}
+
+if(emailField.value.match(validation)){
+    emailError.innerHTML = "";
+    emailField.style.borderBottomColor= "green";
+    return true;
+}
+
+}
+
+
+
+
 
 
