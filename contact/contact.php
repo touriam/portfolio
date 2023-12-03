426679b140b3613cd27e8d855f57f203a3bb18f9
touriam <90021920+touriam@users.noreply.github.com>
2023-12-03 18:58:14 -0500

    adding variables to php form

--- contact/contact.php
+++ contact/contact.php
@@ -1,7 +1,20 @@
 <?php
 
-$name =$_POST['name'];
-$email =$_POST['email'];
-$number =$_POST['number'];
-$message =$_POST['message'];
+ 
+if(isset($_POST['submit'])){
+    $name =$_POST['name'];
+    $emailFrom =$_POST['email'];
+    $number =$_POST['number'];
+    $message =$_POST['message'];
+    $submit =$_POST['submit'];
+    
+    $mailTo=""
+    $headers= "From: ".mailFrom;
+    $txt "you have received an email-from ".$name.".\n\n".$message;
+
+    mail($mailTo, $number, $txt, $headers);
+    header("Location: index.html?mailsend")
+
+}
+
 ?>
