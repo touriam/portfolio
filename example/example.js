7da98d761e7ffb9b04d9385e47b463ab0c9f396f
touriam <90021920+touriam@users.noreply.github.com>
2023-12-05 23:09:10 -0500

    fixes

---
+++ example/example.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const listItem = document.querySelectorAll('.list');
+const navContainer = document.querySelector('.nav-container');
+const mediaQuery = window.matchMedia('(max-width: 800px)')
+
+
+hamburger.addEventListener("click", function(){
+    console.log("im working");
+    if (navContainer.style.display ==="none"){
+        navContainer.style.display = "flex";
+        hamburger.style.display ="flex"
+    }
+    
+    else (navContainer.style.display = "none")
+    
+    })
+
+
